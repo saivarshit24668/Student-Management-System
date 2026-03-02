06e0c68e4d261b1e4d923bed850bb8e9b7e13cb7
Sai Varshit Kathula <saivarshit.2008@gmail.com>
2026-03-02 19:19:31 +0530

    added StudentService class with ArrayList<Student> and empty  method skeletons

--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,5 +1,24 @@
 package service;
-
+import model.Student;
+import java.util.*;
 public class StudentService {
-    
+    ArrayList<Student> students;
+    public StudentService(){
+        students=new ArrayList<>();
+    }
+    public void addStudent(){
+
+    }
+    public void viewStudents(){
+
+    }
+    public void searchById(){
+
+    }
+    public void UpdateStudentDetails(){
+
+    }
+    public void deleteStudent(){
+
+    }
 }
